6c6d68297b7ff5ea34989de2e878edcff5621a41
kuba <kuban.kurmanaliev.job@gmail.com>
2019-02-15 10:36:20 +0600

    Fix routes.

--- resources/stubs/module/Routes/web.php
+++ resources/stubs/module/Routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 Route::localizedGroup(function () {
-    Route::prefix(config('cms.uri'))
-        ->as(config('cms.uri-alias'))
-        ->group(function (){
-            Route::resource('DummySlug', 'Admin\IndexController');
-            //Route::delete('DummySlug/delete-upload/{id}/{field}', 'Admin\IndexController@deleteUpload')->name('DummySlug.delete-upload');
-        });
+    Route::prefix(config('cms.uri'))->as(config('cms.admin_prefix'))->group(function () {
+        Route::resource('DummySlug', 'Admin\IndexController');
+        /*Route::delete('DummySlug/delete-upload/{id}/{field}', 'Admin\IndexController@deleteUpload')
+            ->name('DummySlug.delete-upload');*/
+    });
 });
